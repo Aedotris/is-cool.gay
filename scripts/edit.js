68b355b6df254f9010e4868fb041057dd5d4bcfd
Aedotris <lmd.x.dev@gmail.com>
2024-07-20 13:19:54 +0700

    Update edit.js

--- scripts/edit.js
+++ scripts/edit.js
@@ -1,38 +1,43 @@
 const array = process.env.EVENT_ISSUE_BODY.split("### ");
 const nonascii = /[^\u0000-\u007F]+/;
+
+// Split each item by double newlines
 array.forEach((item, index) => {
   array[index] = item.split("\n\n");
 });
+
+// Handle additional newlines in the third section
 const original = array[3][1];
 array[3][1] = original.split("\n")[0];
 array[3].push(original.split("\n")[1]);
 
-// Kiểm tra định dạng đầu vào
+// Check input format
 if (
   !(
-    array.length == 4 &&
-    array[0].length == 1 &&
-    array[0][0] == "" &&
-    array[1][0] == "Subdomain Name" &&
-    array[1].length == 3 &&
-    array[2][0] == "DNS Record" &&
-    array[2].length == 3 &&
-    array[3].length == 3 &&
-    array[3][0] == "Agreement" &&
-    array[3][1] == "- [X] I have ensured that this subdomain is mine" &&
-    array[3][2] == undefined &&
+    array.length === 4 &&
+    array[0].length === 1 &&
+    array[0][0] === "" &&
+    array[1][0] === "Subdomain Name" &&
+    array[1].length === 3 &&
+    array[2][0] === "DNS Record" &&
+    array[2].length === 3 &&
+    array[3].length === 3 &&
+    array[3][0] === "Agreement" &&
+    array[3][1] === "- [X] I have ensured that this subdomain is mine" &&
+    array[3][2] === undefined &&
     !array[1][1].includes(" ") &&
     !array[2][1].includes(" ") &&
     !nonascii.test(array[1][1]) &&
     !nonascii.test(array[2][1]) &&
-    array[1][1] != ".is-cool.gay" &&
+    array[1][1] !== ".is-cool.gay" &&
     array[1][1].endsWith(".is-cool.gay")
   )
 ) {
-  return console.log(
+  console.log(
     "not planned|Format invalid! It's usually because you didn't check the agreements, or the domain/record you entered is invalid!|" +
-      array[1][1]
+    array[1][1]
   );
+  return;
 }
 
 const flare = require("cloudflare");
@@ -57,26 +62,29 @@ cf.dns.records
   .then((records) => {
     const availabilityFilter = records.result.filter((record) => {
       return (
-        record.name == array[1][1] &&
-        record.comment == process.env.EVENT_USER_LOGIN
+        record.name === array[1][1] &&
+        record.comment === process.env.EVENT_USER_LOGIN
       );
     });
-    if (availabilityFilter[0]) {
+
+    if (availabilityFilter.length > 0) {
       let type = "invalid";
       if (hostname.test(array[2][1])) type = "CNAME";
       if (ipv4.test(array[2][1])) type = "A";
       if (ipv6.test(array[2][1])) type = "AAAA";
       if (type === "invalid") {
-        return console.log(
+        console.log(
           "not planned|The record destination you entered is invalid!|" +
-            array[2][1]
+          array[2][1]
         );
+        return;
       }
+
       cf.dns.records
         .edit(
           availabilityFilter[0].id,
-          { zone_id: "66b0d2af52cc381a6a0b04fcd4b9c83d" },
           {
+            zone_id: "66b0d2af52cc381a6a0b04fcd4b9c83d",
             content: array[2][1],
             name: array[1][1],
             proxied: false,
@@ -90,19 +98,20 @@ cf.dns.records
             const errorMessage = response.errors && response.errors.length > 0
               ? response.errors[0].message
               : "Unknown error";
-            return console.log(
+            console.log(
               `not planned|CloudFlare Error:${errorMessage}|${array[1][1]}`
             );
+            return;
           }
-          return console.log(
+          console.log(
             "completed|Your subdomain has been successfully edited!|" +
-              array[1][1]
+            array[1][1]
           );
         });
     } else {
-      return console.log(
+      console.log(
         "not planned|This subdomain is not yours or the subdomain is not found!|" +
-          array[1][1]
+        array[1][1]
       );
     }
   })
